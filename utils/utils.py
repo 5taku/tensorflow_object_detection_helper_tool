a9c26bb6b0a6b53eb22b02ed9dbc8ed1cbe9068e
5taku <ksulki@gmail.com>
2018-05-06 12:31:45 +0900

    utils, retraining, tfgenerator 로직 정리
    Signed-off-by: 5taku <ksulki@gmail.com>

--- utils/utils.py
+++ utils/utils.py
@@ -1,26 +1,56 @@
 import os
 import tarfile
+import fileinput
+import sys
 from tqdm import tqdm
 import requests
 import math
+from object_detection.utils import label_map_util
 
-model_dict = {1:'ssd_mobilenet_v1_coco_2017_11_17' ,
-              2:'ssd_mobilenet_v2_coco_2018_03_29' ,
-              3:'ssd_inception_v2_coco_2017_11_17' ,
-              4:'faster_rcnn_inception_v2_coco_2018_01_28' ,
-              5:'faster_rcnn_resnet50_coco_2018_01_28' ,
-              6:'faster_rcnn_resnet50_lowproposals_coco_2018_01_28' ,
-              7:'rfcn_resnet101_coco_2018_01_28' ,
-              8:'faster_rcnn_resnet101_coco_2018_01_28' ,
-              9:'faster_rcnn_resnet101_lowproposals_coco_2018_01_28' ,
-              10:'faster_rcnn_inception_resnet_v2_atrous_coco_2018_01_28' ,
-              11:'faster_rcnn_inception_resnet_v2_atrous_lowproposals_coco_2018_01_28' ,
-              12:'faster_rcnn_nas_2018_01_28' ,
-              13:'faster_rcnn_nas_lowproposals_coco_2018_01_28'}
+global model_dict
+model_dict= {1:['ssd_mobilenet_v1_coco_2017_11_17', 'ssd_mobilenet_v1_coco.config'],
+              2:['ssd_mobilenet_v2_coco_2018_03_29' ,'ssd_mobilenet_v2_coco.config'] ,
+              3:['ssd_inception_v2_coco_2017_11_17' ,'ssd_inception_v2_coco.config' ],
+              4:['faster_rcnn_inception_v2_coco_2018_01_28','faster_rcnn_inception_v2_coco.config' ],
+              5:['faster_rcnn_resnet50_coco_2018_01_28','faster_rcnn_resnet50_coco.config' ],
+              6:['faster_rcnn_resnet50_lowproposals_coco_2018_01_28','faster_rcnn_resnet50_coco.config' ],
+              7:['rfcn_resnet101_coco_2018_01_28','rfcn_resnet101_coco.config' ],
+              8:['faster_rcnn_resnet101_coco_2018_01_28' ,'faster_rcnn_resnet101_coco.config'],
+              9:['faster_rcnn_resnet101_lowproposals_coco_2018_01_28','faster_rcnn_resnet101_coco.config' ],
+              10:['faster_rcnn_inception_resnet_v2_atrous_coco_2018_01_28','faster_rcnn_inception_resnet_v2_atrous_coco.config' ],
+              11:['faster_rcnn_inception_resnet_v2_atrous_lowproposals_coco_2018_01_28','faster_rcnn_inception_resnet_v2_atrous_coco.config' ],
+              12:['faster_rcnn_nas_2018_01_28','faster_rcnn_nas_coco.config' ],
+              13:['faster_rcnn_nas_lowproposals_coco_2018_01_28','faster_rcnn_nas_coco.config']}
+
+def model_input():
+    print("+++++++++++++++++++++++++++++++++++")
+    print("++++++ Auto re training tool ++++++")
+    print("++++++        5TAKU          ++++++")
+    print("+++++++++++++++++++++++++++++++++++")
+    print("")
+    print("Step 1 Choose model ")
+    print("")
+    print("1. ssd_mobilenet_v1_coco ")
+    print("2. ssd_mobilenet_v2_coco ")
+    print("3. ssd_inception_v2_coco ")
+    print("4. faster_rcnn_inception_v2_coco ")
+    print("5. faster_rcnn_resnet50_coco ")
+    print("6. faster_rcnn_resnet50_lowproposals_coco ")
+    print("7. rfcn_resnet101_coco ")
+    print("8. faster_rcnn_resnet101_coco ")
+    print("9. faster_rcnn_resnet101_lowproposals_coco ")
+    print("10. faster_rcnn_inception_resnet_v2_atrous_coco ")
+    print("11. faster_rcnn_inception_resnet_v2_atrous_lowproposals_coco ")
+    print("12. faster_rcnn_nas ")
+    print("13. faster_rcnn_nas_lowproposals_coco ")
+    print("")
+
+    model = int(input('Select Model Number : '))
+
+    return model
 
 def download_model(modelnum):
-
-    MODEL_NAME = model_dict[modelnum]
+    MODEL_NAME = model_dict[modelnum][0]
     DIC_NAME = './model_zoo/'
     MODEL_DIC_NAME = DIC_NAME + MODEL_NAME
     MODEL_FILE = MODEL_NAME + '.tar.gz'
@@ -29,6 +59,9 @@ def download_model(modelnum):
 
     if not os.path.isdir(MODEL_DIC_NAME):
         print(MODEL_NAME + " Model not Exist. Download start.")
+
+        # make taring_dir folder
+        os.mkdir('./tarin_dir/'+MODEL_NAME)
 
         # Streaming, so we can iterate over the response.
         r = requests.get(DOWNLOAD_BASE + MODEL_FILE, stream=True)
@@ -50,26 +83,42 @@ def download_model(modelnum):
             if 'frozen_inference_graph.pb' in file_name:
                 tar_file.extract(file, DIC_NAME )
 
-
 def remove_model_tar_file(modelnum):
-
-    MODEL_NAME = model_dict[modelnum]
+    MODEL_NAME = model_dict[modelnum][0]
     DIC_NAME = './model_zoo/'
     MODEL_DIC_NAME = DIC_NAME + MODEL_NAME
-    MODEL_FILE = MODEL_NAME + '.tar.gz'
     FULL_FILE_PATH = MODEL_DIC_NAME + '.tar.gz'
 
     if os.path.exists(FULL_FILE_PATH):
         os.remove(FULL_FILE_PATH)
 
-def change_exam_count(modelnum, examnum):
-    MODEL_NAME = model_dict[modelnum]
-    MODEL_CONF_NAME = MODEL_NAME[0:MODEL_NAME.find('_201')] + '.config'
-    f = open('./model_conf/'+MODEL_CONF_NAME)
+def remake_config(model,exam_num,record):
+    config_file = './model_conf/'+model_dict[model][1]
+    model_checkpoint_path = './tarin_dir/'+model_dict[model][0]+'/model.ckpt'
+    num_steps = 'num_steps: '
+    num_classes = 'num_classes: '
+    label_map_path = 'label_map_path: '
+    fine_tune_checkpoint = 'fine_tune_checkpoint: '
 
-    while True:
-        line = f.readline()
-        if "num_examples" in line:
-            print("Okay")
-        if not line: break
-    f.close()
+    #get_class_num
+    #TODO : more smarter logic
+    for arguments in record:
+         if arguments['label_file']:
+            label_file = arguments['label_file']
+    class_num = 0
+    for line in fileinput.input(label_file, inplace=1):
+        if 'id: ' in line:
+            class_num += 1
+        sys.stdout.write(line)
+
+    for line in fileinput.input(config_file, inplace=1):
+        if num_steps in line:
+            line = line.replace(line,'  '+num_steps + str(exam_num) + '\n')
+        if num_classes in line:
+            line = line.replace(line,'  '+num_classes + str(class_num) + '\n')
+        if label_map_path in line:
+            line = line.replace(line, label_map_path + '"' + label_file + '"' + '\n')
+        if fine_tune_checkpoint in line:
+            line = line.replace(line, fine_tune_checkpoint + '"' + model_checkpoint_path + '"' + '\n')
+        sys.stdout.write(line)
+
